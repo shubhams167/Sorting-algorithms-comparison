899b4c6d18d95fb216cc8e197ce5c2daa853cd97
Shubham Singh <shubhams.167@gmail.com>
2018-11-09 03:37:22 +0530

    .h to .hpp

--- SortingAlgorithmComparison.cpp
+++ SortingAlgorithmComparison.cpp
@@ -3,11 +3,11 @@
 #include<ctime>
 #include<fstream>
 #include<chrono>
-#include "selectionSort.h"
-#include "insertionSort.h"
-#include "bubbleSort.h"
-#include "quickSort.h"
-#include "mergeSort.h"
+#include "selectionSort.hpp"
+#include "insertionSort.hpp"
+#include "bubbleSort.hpp"
+#include "quickSort.hpp"
+#include "mergeSort.hpp"
 
 using namespace std;
 
@@ -25,69 +25,69 @@ int main()
 	int *arr;
 	long long comparisons;
 	
-	/*Test bubble sort*/
-	o.open("bubbleSortResult.csv");
-	for(int size = 500; size <= 20000; size += 500)
-	{
-		comparisons = 0;
-		arr = new int[size];//Allocate memory for array with size elements
-		randomize(arr, size);//Initialize array with random values
-		auto start = chrono::high_resolution_clock::now();//Start counting time
-		
-		bubbleSort(arr, size, comparisons);//Call bubbleSort to sort randomized array
-		
-		auto end = chrono::high_resolution_clock::now();//Stop counting time
-		chrono::duration<float> time_elapsed = end - start;//Get time difference
-		float time_taken = time_elapsed.count()*1000;//Time in milliseconds
-		o << size << "," << time_taken << "," << comparisons << '\n';
-		delete[] arr;//Free memory allocated for the array
-	}
-	o.close();
-	/*Bubble sort testing complete*/
-	
-	/*Test insertion sort*/
-	o.open("insertionSortResult.csv");
-	for(int size = 500; size <= 20000; size += 500)
-	{
-		comparisons = 0;
-		arr = new int[size];//Allocate memory for array with size elements
-		randomize(arr, size);//Initialize array with random values
-		auto start = chrono::high_resolution_clock::now();//Start counting time
-		
-		insertionSort(arr, size, comparisons);//Call insertionSort to sort randomized array
-		
-		auto end = chrono::high_resolution_clock::now();//Stop counting time
-		chrono::duration<float> time_elapsed = end - start;//Get time difference
-		float time_taken = time_elapsed.count()*1000;//Time in milliseconds
-		o << size << "," << time_taken << "," << comparisons << '\n';
-		delete[] arr;//Free memory allocated for the array
-	}
-	o.close();
-	/*Insertion sort testing complete*/	
-	
-	/*Test selection sort*/
-	o.open("selectionSortResult.csv");
-	for(int size = 500; size <= 20000; size += 500)
-	{
-		comparisons = 0;
-		arr = new int[size];//Allocate memory for array with size elements
-		randomize(arr, size);//Initialize array with random values
-		auto start = chrono::high_resolution_clock::now();//Start counting time
-		
-		selectionSort(arr, size, comparisons);//Call selectionSort to sort randomized array
-		
-		auto end = chrono::high_resolution_clock::now();//Stop counting time
-		chrono::duration<float> time_elapsed = end - start;//Get time difference
-		float time_taken = time_elapsed.count()*1000;//Time in milliseconds
-		o << size << "," << time_taken << "," << comparisons << '\n';
-		delete[] arr;//Free memory allocated for the array
-	}
-	o.close();
-	/*Selection sort testing complete*/	
+//	/*Test bubble sort*/
+//	o.open("bubbleSortResult.csv");
+//	for(int size = 500; size <= 20000; size += 500)
+//	{
+//		comparisons = 0;
+//		arr = new int[size];//Allocate memory for array with size elements
+//		randomize(arr, size);//Initialize array with random values
+//		auto start = chrono::high_resolution_clock::now();//Start counting time
+//		
+//		bubbleSort(arr, size, comparisons);//Call bubbleSort to sort randomized array
+//		
+//		auto end = chrono::high_resolution_clock::now();//Stop counting time
+//		chrono::duration<float> time_elapsed = end - start;//Get time difference
+//		float time_taken = time_elapsed.count()*1000;//Time in milliseconds
+//		o << size << "," << time_taken << "," << comparisons << '\n';
+//		delete[] arr;//Free memory allocated for the array
+//	}
+//	o.close();
+//	/*Bubble sort testing complete*/
+//	
+//	/*Test insertion sort*/
+//	o.open("insertionSortResult.csv");
+//	for(int size = 500; size <= 20000; size += 500)
+//	{
+//		comparisons = 0;
+//		arr = new int[size];//Allocate memory for array with size elements
+//		randomize(arr, size);//Initialize array with random values
+//		auto start = chrono::high_resolution_clock::now();//Start counting time
+//		
+//		insertionSort(arr, size, comparisons);//Call insertionSort to sort randomized array
+//		
+//		auto end = chrono::high_resolution_clock::now();//Stop counting time
+//		chrono::duration<float> time_elapsed = end - start;//Get time difference
+//		float time_taken = time_elapsed.count()*1000;//Time in milliseconds
+//		o << size << "," << time_taken << "," << comparisons << '\n';
+//		delete[] arr;//Free memory allocated for the array
+//	}
+//	o.close();
+//	/*Insertion sort testing complete*/	
+//	
+//	/*Test selection sort*/
+//	o.open("selectionSortResult.csv");
+//	for(int size = 500; size <= 20000; size += 500)
+//	{
+//		comparisons = 0;
+//		arr = new int[size];//Allocate memory for array with size elements
+//		randomize(arr, size);//Initialize array with random values
+//		auto start = chrono::high_resolution_clock::now();//Start counting time
+//		
+//		selectionSort(arr, size, comparisons);//Call selectionSort to sort randomized array
+//		
+//		auto end = chrono::high_resolution_clock::now();//Stop counting time
+//		chrono::duration<float> time_elapsed = end - start;//Get time difference
+//		float time_taken = time_elapsed.count()*1000;//Time in milliseconds
+//		o << size << "," << time_taken << "," << comparisons << '\n';
+//		delete[] arr;//Free memory allocated for the array
+//	}
+//	o.close();
+//	/*Selection sort testing complete*/	
 	
 	/*Test quick sort*/
 	o.open("quickSortResult.csv");
-	for(int size = 500; size <= 20000; size += 500)
+	for(int size = 500; size <= 100000; size += 500)
 	{
 		comparisons = 0;
 		arr = new int[size];//Allocate memory for array with size elements
@@ -107,7 +107,7 @@ int main()
 	
 	/*Test merge sort*/
 	o.open("mergeSortResult.csv");
-	for(int size = 500; size <= 20000; size += 500)
+	for(int size = 500; size <= 100000; size += 500)
 	{
 		comparisons = 0;
 		arr = new int[size];//Allocate memory for array with size elements
